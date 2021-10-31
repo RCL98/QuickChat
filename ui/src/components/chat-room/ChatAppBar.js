393bcfc4cb667f01703492d523b3751a3472eeaf
RCL98 <48773399+RCL98@users.noreply.github.com>
2021-10-31 22:59:57 +0200

    changed users rendering in chat app bar

--- ui/src/components/chat-room/ChatAppBar.js
+++ ui/src/components/chat-room/ChatAppBar.js
@@ -11,10 +11,34 @@ import GroupsIcon from "@mui/icons-material/Groups";
 import { CONVERSATION } from "../../app/constants";
 
 export default function ChatAppBar() {
-  const users = useSelector((state) => state.users);
-  const currentChat = useSelector((state) => state.profile.currentChat);
-  let chat = useSelector((state) => state.chats.find((chat) => chat.id === currentChat));
+  const profile = useSelector((state) => state.profile);
+  const users = useSelector((state) => state.users).filter((user) => user.id !== profile.userId);
+  let chat = useSelector((state) => state.chats.find((chat) => chat.id === profile.currentChat));
   if (chat === undefined) chat = { name: "You" };
+
+  const renderUsers = () => {
+    if (chat.type !== CONVERSATION)
+      if (users.length <= 3)
+        return (
+          <Typography variant="body1">
+            {users.map((user) => (user.isWriting ? `${user.name} is typing` : user.name)).join(", ")}
+          </Typography>
+        );
+      else {
+        console.log(users.sort((a, b) => (a.isWriting === b.isWriting ? 0 : a.isWriting ? -1 : 1)));
+        return (
+          <Typography variant="body1">
+            {users
+              .sort((a, b) => (a.isWriting === b.isWriting ? 0 : a.isWriting ? -1 : 1))
+              .slice(0, 3)
+              .map((user) => (user.isWriting ? `${user.name} is typing` : user.name))
+              .join(", ") + ", ..."}
+          </Typography>
+        );
+      }
+    else if (users[0].isWriting) return <Typography variant="body1">{`${users[0].name} is typing`}</Typography>;
+    return null;
+  };
 
   return (
     <Stack
@@ -32,9 +56,7 @@ export default function ChatAppBar() {
       <Avatar alt="Profile Pic">{chat.type === CONVERSATION ? <AccountCircleIcon /> : <GroupsIcon />}</Avatar>
       <Stack direction="column">
         <Typography variant="h5">{chat.name}</Typography>
-        <Typography variant="body1">
-          {users.map((user) => (user.isWriting ? `${user.name} is typing` : user.name)).join(", ")}
-        </Typography>
+        {renderUsers()}
       </Stack>
     </Stack>
   );
