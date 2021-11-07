6069685586375843bc6126f2feaf4418611dc682
iulius98 <iuliuscazoni98@gmail.com>
2021-11-07 22:45:51 +0200

    add delete capability
    Change-Id: I67644380a7a6bff2691ba361fd05120872806ffd
    Signed-off-by: iulius98 <iuliuscazoni98@gmail.com>

--- server/src/main/java/com/circ/quickchat/controller/PhotoController.java
+++ server/src/main/java/com/circ/quickchat/controller/PhotoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,16 @@ public class PhotoController {
 	public void uploadPhotoGroup(@RequestParam("file") MultipartFile file,
 			@RequestParam("groupId") Long groupId, @RequestParam("sessionId") String sessionId) throws IOException {
 		photoService.uploadPhotoForGroup(groupId, file, sessionId);
+	}
+	
+	@DeleteMapping("/group/{groupId}/{sessionId}")
+	public void deletePhotoGroup(@PathVariable Long groupId, @PathVariable String sessionId) {
+		photoService.deletePhotoForGroup(groupId, sessionId);
+	}
+	
+	@DeleteMapping("/user/{sessionId}")
+	public void deletePhototForUser(@PathVariable String sessionId) {
+		photoService.deletePhototForUser(sessionId);
 	}
 	
 	@GetMapping("/group/get/{groupId}")
--- server/src/main/java/com/circ/quickchat/service/PhotoService.java
+++ server/src/main/java/com/circ/quickchat/service/PhotoService.java
@@ -113,6 +113,57 @@ public class PhotoService {
 		
 	}
 	
+	public void deletePhotoForGroup(Long groupId, String sessionId) {
+		Group group = groupService.getGroupById(groupId);
+		Photo photo = group.getPhoto();
+		group.setPhoto(photo);
+		groupService.save(group);
+		deletePhoto(photo);
+		WebsocketMessage websocketMessage = WebsocketMessage.builder()
+				.content(groupId).messageType(MessageType.UPDATE_GROUP_PHOTO)
+				.build();
+		userUtilCommun.sendToUsers(websocketMessage, group.getChat().getUsers()
+				.stream().map(usr -> usr.getSessionId())
+				.filter(filSessionId -> !filSessionId.equals(sessionId))
+				.collect(Collectors.toList()));
+	}
+	
+	public void deletePhototForUser(String sessionId) {
+		User usr = userService.getUserBySessionId(sessionId);
+		Photo photo = usr.getPhoto();
+		usr.setPhoto(null);
+		userService.save(usr);
+		deletePhoto(photo);
+		List<Map<String, Object>> convsInfo = conversationService.findAll()
+				.stream().filter(conv -> {
+					List<Long> users = conv.getConversationsInfo().stream().map(cInfo -> cInfo.getUserId())
+							.collect(Collectors.toList());
+					Boolean isGoodConv = users.stream().anyMatch(userId -> userId.equals(usr.getId()));
+					return isGoodConv;
+				}).map(conv -> {
+					Map<String, Object> infoConvMap = new HashMap<String, Object>();
+					List<Long> users = conv.getConversationsInfo().stream().map(cInfo -> cInfo.getUserId())
+							.collect(Collectors.toList());
+					Long userId  = users.stream().filter(usrId -> !usrId.equals(usr.getId())).findAny().get();
+					infoConvMap.put("userId", userId);
+					infoConvMap.put("convId", conv.getId());
+					return infoConvMap;
+				}).collect(Collectors.toList());
+		
+		convsInfo.forEach(infoConvMap -> {
+			String sessionId2 = userService.getUserForId((Long)infoConvMap.get("userId")).getSessionId();
+			Map<String, Long> messageMap = new HashMap<String, Long>();
+			messageMap.put("userId", usr.getId());
+			messageMap.put("convId", (Long)infoConvMap.get("convId"));
+			WebsocketMessage websocketMessage = WebsocketMessage.builder().messageType(MessageType.UPDATE_USER_PHOTO)
+					.content(messageMap).build();
+			userUtilCommun.sendToUser(sessionId2, websocketMessage);
+
+		});
+		
+	}
+	
+	
 	public byte[] getPhotoForGroup(Long groupId) throws IOException {
 		Group group = groupService.getGroupById(groupId);
 		Photo photo = group.getPhoto();
@@ -133,8 +184,10 @@ public class PhotoService {
 	private Photo savePhoto(MultipartFile file) throws IOException {
 		String photoUri = photoDirectoryPath + "/" + UUID.randomUUID();
 		File photoFile = new File(photoUri);
-		Files.copy(file.getInputStream(),photoFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		InputStream inFile = file.getInputStream();
+		Files.copy(inFile,photoFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		Photo photo = Photo.builder().bigPhotoUri(photoUri).build();
+		inFile.close();
 		return photoRepository.save(photo);
 	}
 	
@@ -143,6 +196,7 @@ public class PhotoService {
 		InputStream in = new FileInputStream(photoFile);
 		byte[] imagesBytes = new byte[in.available()];
 		in.read(imagesBytes);
+		in.close();
 		return imagesBytes;
 	}
 	
